f3da72637c95e2e011e6f06bd6a792dd15493eb7
Gerald Büchler <dev@genomast.de>
2019-11-25 12:01:36 +0100

    Layout for "split" is ready. Exit is already working.

--- frmmain.cpp
+++ frmmain.cpp
@@ -6,6 +6,8 @@ FrmMain::FrmMain(QWidget *parent)
     , ui(new Ui::FrmMain)
 {
     ui->setupUi(this);
+    connect(ui->actionExit,&QAction::triggered,this,&FrmMain::close);
+    connect(ui->btnExit, &QPushButton::clicked,this,&FrmMain::close);
 }
 
 FrmMain::~FrmMain()
